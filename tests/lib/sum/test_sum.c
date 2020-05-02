80921078390a0f0e4919eb22bdb1f11c0dc3f080
Pietro Panizza <pietropanizza88@gmail.com>
2020-05-02 17:59:14 +0200

    Init

---
+++ tests/lib/sum/test_sum.c
@@ -0,0 +1,26 @@
+#include "unity.h"
+#include "sum.h"
+
+void sum_positive() {
+    TEST_ASSERT_EQUAL_INT(3, sum(1,2));
+    TEST_ASSERT_EQUAL_INT(13, sum(10,3));
+}
+
+void sum_negative() {
+    TEST_ASSERT_EQUAL_INT(-3, sum(-1,-2));
+    TEST_ASSERT_EQUAL_INT(-5, sum(-1,-4));
+}
+
+void sum_zero() {
+    TEST_ASSERT_EQUAL_INT(0, sum(0,0));
+    TEST_ASSERT_EQUAL_INT(5, sum(0,5));
+    TEST_ASSERT_EQUAL_INT(-3, sum(-3,0));
+}
+
+int main() {
+    UNITY_BEGIN();
+    RUN_TEST(sum_positive);
+    RUN_TEST(sum_negative);
+    RUN_TEST(sum_zero);
+    return UNITY_END();
+}
